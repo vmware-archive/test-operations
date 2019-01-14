9aa6526520bc8dee693fe6dd7fd377e0fc096c1f
Glenn McElhoe <gmcelhoe@vmware.com>
2019-01-14 18:16:08 -0500

    Script needs to return 0 (no error)
    Not having the signing certificates is a non-fatal error.

--- build-resources/extract_jar_signing_artifacts.sh
+++ build-resources/extract_jar_signing_artifacts.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -e
 
-${1:?"encryption key is required for creating signed jars"}
-${2:?"initialization vector is required for creating signed jars"}
+if [ -z "$1" ] || [ -z "$2" ]
+then
+  echo "Encryption key and initialization vector are required for creating signed jars"
 
-openssl aes-256-cbc -K $encrypted_e586968263a2_key -iv $encrypted_e586968263a2_iv -in build-resources/gpg_files.tar.enc -out gpg_files.tar -d
-tar -f gpg_files.tar -xO gpg-secret-keys | $GPG_EXECUTABLE --import
-tar -f gpg_files.tar -xO gpg-ownertrust | $GPG_EXECUTABLE --import-ownertrust
+else
+
+  openssl aes-256-cbc -K $encrypted_e586968263a2_key -iv $encrypted_e586968263a2_iv -in build-resources/gpg_files.tar.enc -out gpg_files.tar -d
+  tar -f gpg_files.tar -xO gpg-secret-keys | $GPG_EXECUTABLE --import
+  tar -f gpg_files.tar -xO gpg-ownertrust | $GPG_EXECUTABLE --import-ownertrust
+
+fi
